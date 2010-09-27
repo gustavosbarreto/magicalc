7c76d624953c2143092d9775ca67a155fb75e7c8
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2010-09-27 18:28:17 -0300

    Untabify the file

--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,17 @@
 
 int main(int argc, char *argv[])
 {
-	QApplication app(argc, argv);
+    QApplication app(argc, argv);
 
-	QFontDatabase::addApplicationFont(":/resources/font.ttf");
+    QFontDatabase::addApplicationFont(":/resources/font.ttf");
 
     QTranslator qtTranslator;
     qtTranslator.load("qt_" + QLocale::system().name(),
-                       QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+                      QLibraryInfo::location(QLibraryInfo::TranslationsPath));
     app.installTranslator(&qtTranslator);
 
-	MainWindow *mainWindow = new MainWindow;
+    MainWindow *mainWindow = new MainWindow;
     mainWindow->show();
-//    mainWindow->move(1000, 1000);
 
-	return app.exec();
+    return app.exec();
 }
